30469d7d5a1ace321031eca3b8fbe07f86b9cace
farag2 <farag2@users.noreply.github.com>
2020-03-19 11:39:58 +0300

    Add files via upload

--- Drivers.ps1
+++ Drivers.ps1
@@ -6,7 +6,7 @@ $Date = @{
 	Name = "Date"
 	Expression = {$_.Date.Tostring().Split("")[0]}
 }
-$AllDrivers = Get-WindowsDriver –Online -All | Where-Object -FilterScript {$_.Driver -like 'oem*inf'} | Select-Object -Property $OriginalFileName, Driver, ClassDescription, ProviderName, $Date, Version
+$AllDrivers = Get-WindowsDriver -Online -All | Where-Object -FilterScript {$_.Driver -like 'oem*inf'} | Select-Object -Property $OriginalFileName, Driver, ClassDescription, ProviderName, $Date, Version
 Write-Host "`nAll installed third-party drivers" -ForegroundColor Yellow
 ($AllDrivers | Sort-Object -Property ClassDescription | Format-Table -AutoSize -Wrap | Out-String).Trim()
 $DriverGroups = $AllDrivers | Group-Object -Property OriginalFileName | Where-Object -FilterScript {$_.Count -gt 1}
