b2e5e12f397a29ce9e61105be3d316267b49c302
farag2 <farag2@users.noreply.github.com>
2020-05-22 00:13:29 +0300

    Add files via upload

--- Drivers.ps1
+++ Drivers.ps1
@@ -7,14 +7,18 @@ $Date = @{
 	Expression = {$_.Date.Tostring().Split("")[0]}
 }
 $AllDrivers = Get-WindowsDriver -Online -All | Where-Object -FilterScript {$_.Driver -like 'oem*inf'} | Select-Object -Property $OriginalFileName, Driver, ClassDescription, ProviderName, $Date, Version
-Write-Host "`nAll installed third-party drivers" -ForegroundColor Yellow
+
+Write-Verbose "`nAll installed third-party drivers" -Verbose
 ($AllDrivers | Sort-Object -Property ClassDescription | Format-Table -AutoSize -Wrap | Out-String).Trim()
 $DriverGroups = $AllDrivers | Group-Object -Property OriginalFileName | Where-Object -FilterScript {$_.Count -gt 1}
-Write-Host "`nDuplicate drivers" -ForegroundColor Yellow
+
+Write-Verbose "`nDuplicate drivers" -Verbose
 ($DriverGroups | ForEach-Object -Process {$_.Group | Sort-Object -Property Date -Descending | Select-Object -Skip 1} | Format-Table | Out-String).Trim()
 $DriversToRemove = $DriverGroups | ForEach-Object -Process {$_.Group | Sort-Object -Property Date -Descending | Select-Object -Skip 1}
-Write-Host "`nDrivers to remove" -ForegroundColor Yellow
+
+Write-Verbose "`nDrivers to remove" -Verbose
 ($DriversToRemove | Sort-Object -Property ClassDescription | Format-Table | Out-String).Trim()
+
 foreach ($item in $DriversToRemove)
 {
 	$Name = $($item.Driver).Trim()
